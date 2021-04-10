a0db4aa4be039c001daf977a94fe7089f8d66dfd
Oğuz Evirgen <59138609+evirgenoguz@users.noreply.github.com>
2021-04-10 16:19:44 +0300

    18. Gün Ders Sonu

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -20,7 +20,7 @@ namespace DataAccess.Concrete.EntityFramework
             {
                 var result = from p in context.Products
                              join c in context.Categories
-                             on p.CategoryID equals c.CategoryID
+                             on p.CategoryID equals c.CategoryId
                              select new ProductDetailDto { ProductId = p.ProductID, ProductName = p.ProductName, CategoryName = c.CategoryName, UnitsInStock = p.UnitsInStock };
                 return result.ToList();
             }
